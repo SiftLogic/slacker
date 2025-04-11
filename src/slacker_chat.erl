b0a947e06d098dc37d9c29eb96e50de204d31a70
Leonard Boyce <leonard@verias.com>
2025-04-11 09:53:30 -0400

    Support new required authorization: bearer header

--- src/slacker_chat.erl
+++ src/slacker_chat.erl
@@ -43,7 +43,7 @@ post_message(Token, Channel, Message, Options) ->
 -spec post_message_bearer(Token :: string(), Channel :: string(), Message :: string(), Options :: list()) -> http_response().
 post_message_bearer(Token, Channel, Message, Options) ->
     Headers = [{"authorization", "bearer " ++ Token}],
-    slacker_request:send(post, "chat.postMessage", Headers, [{"channel", Channel},{"text", Message}], Options).
+    slacker_request:send(post, "chat.postMessage", Headers, [{"channel", Channel},{"text", Message}] ++ Options, Options).
 
 %% @doc Updates a message in a channel.
 %%
